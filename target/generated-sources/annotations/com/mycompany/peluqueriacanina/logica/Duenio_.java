f9be19cd1908e1f0f36c79fc8a587249d401e731
diegoceballos95 <diegoceballos95@yahoo.com>
2024-01-22 01:52:35 -0300

    Primer commit del proyecto finalizado

---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Duenio_.java
@@ -0,0 +1,17 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import com.mycompany.peluqueriacanina.logica.Mascota;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-01-19T18:54:04", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Duenio.class)
+public class Duenio_ { 
+
+    public static volatile SingularAttribute<Duenio, String> celDuenio;
+    public static volatile SingularAttribute<Duenio, Integer> numDuenio;
+    public static volatile SingularAttribute<Duenio, Mascota> mascota;
+    public static volatile SingularAttribute<Duenio, String> nombre;
+
+}
